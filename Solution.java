05304a677c5e0663947054fa367a6dcc190fe25d
Karina Telles <karinac.telles@gmail.com>
2025-02-01 20:54:09 -0300

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -6,11 +6,12 @@ public class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String line = "";
-
+        
         for(int i=1;scanner.hasNext()==true;i++){
             scanner.hasNext();
             line = scanner.nextLine();
             System.out.println(i + " " + line);
         }
+        scanner.close();
     }
 }
